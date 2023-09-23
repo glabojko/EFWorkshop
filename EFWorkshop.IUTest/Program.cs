91fd50f0a00785896fa50a90905413de39b4a3f2
glabojko <glabojko@gmail.com>
2023-09-23 14:26:26 +0200

    Dodaj pliki projektów.

---
+++ EFWorkshop.IUTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFWorkshop.IUTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
