a902b6fd03f3e70d7ee0d05d8d1d2b83c7025170
glabojko <glabojko@gmail.com>
2023-09-23 15:49:55 +0200

    database added

---
+++ EFWorkshop.Data/CompanyContext.cs
@@ -0,0 +1,28 @@
+﻿using EFWorkshop.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFWorkshop.Data
+{
+    public class CompanyContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=DESKTOP-FLUTGQS\\SQLEXPRESS;Database=CompanyDB;Integrated Security=true;TrustServerCertificate=true";
+        
+        public DbSet<Employee> Employees { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+
+
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- EFWorkshop.IUTest/Program.cs
+++ EFWorkshop.IUTest/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace EFWorkshop.IUTest
+﻿using EFWorkshop.Data;
+using EFWorkshop.Domain;
+
+namespace EFWorkshop.IUTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var companyContext = new CompanyContext();
+            var employees = companyContext.Employees;
+
+            foreach (var employee in employees)
+            { 
+                Console.WriteLine(employee);
+            }
         }
     }
 }
