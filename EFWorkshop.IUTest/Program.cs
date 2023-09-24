363e6a0ba90813e21b4fa5f07e5cbb8dda648c91
glabojko <glabojko@gmail.com>
2023-09-24 11:03:32 +0200

    adding and removingfrom database

--- EFWorkshop.IUTest/Program.cs
+++ EFWorkshop.IUTest/Program.cs
@@ -16,9 +16,18 @@ namespace EFWorkshop.IUTest
             // var e = employees.FirstOrDefault(x => x.Id == 2);
             // var e2 = employees.FirstOrDefault(x => x.Id == 2);
             // var e3 = employees.FirstOrDefault(x => x.Id == 2);
-            var e4 = employees.Find(2);
-            var e5 = employees.Find(2);
-            var e6 = employees.Find(2);
+            // var e4 = employees.Find(2);
+            // var e5 = employees.Find(2);
+            // var e6 = employees.Find(2);
+            //var b = employees.Find(4);
+            //b.FirstName = "Marian";
+            //var emp = new Employee { FirstName = "Dżon", LastName = "Lennon", Salary = "4000" };
+            //companyContext.Employees.Add(emp);
+
+            var c = employees.First(x => x.Id == 5);
+            companyContext.Employees.Remove(c);
+
+            companyContext.SaveChanges();
 
             //foreach (var employee in employees)
             //{ 
