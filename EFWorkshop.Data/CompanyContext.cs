3bc3bc857dbb28b73414ef77a910beaacfc40e1e
glabojko <glabojko@gmail.com>
2023-09-24 11:36:31 +0200

    adding and removingfrom database commit

--- EFWorkshop.Data/CompanyContext.cs
+++ EFWorkshop.Data/CompanyContext.cs
@@ -11,7 +11,7 @@ namespace EFWorkshop.Data
 {
     public class CompanyContext : DbContext
     {
-        private const string ConnectionString = "Data Source=DESKTOP-FLUTGQS\\SQLEXPRESS;Database=CompanyDB;Integrated Security=true;TrustServerCertificate=true";
+        private const string ConnectionString = "Data Source=DESKTOP-FLUTGQS\\SQLEXPRESS;Database=Company;Integrated Security=true;TrustServerCertificate=true";
         
         public DbSet<Employee> Employees { get; set; }
         
